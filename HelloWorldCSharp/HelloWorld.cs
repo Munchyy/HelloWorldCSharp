ecb2780f4c3c0678332c621136dce44ea77e8da5
Munchyy <max.pickup94@gmail.com>
2016-12-07 17:52:14 +0000

    Add project files.

---
+++ HelloWorldCSharp/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿
+namespace HelloWorldCSharp
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadKey();
+        }
+    }
+}
